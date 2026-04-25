558df992e55e3cd870b9dbd4814bac42885c63a8
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:06:16 +0300

    C# OOP BankAccount Classs

---
+++ BACK_END/C#/C#_OOP_BankAccount_Classs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { }
+}
+
+public class BankAccount
+{
+    private decimal balance;
+
+    public decimal getBalance
+    {
+        get {  return balance; } 
+    }
+    public void deposit (int n)
+    {
+        balance += n; 
+    }
+    public void withdraw (int n)
+    {
+        if (balance>n)
+        {
+            balance -= n;
+        }
+    }
+}
