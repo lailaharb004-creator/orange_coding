8ffb6fb5ab0a0caddbe4dd7b9e703149d6e04777
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:19:58 +0300

    C# OOP Company Class

---
+++ BACK_END/C#/C#_OOP_Company_Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { 
+        Company c1 = new Company();
+        Company c2 = new Company();
+        Company c3 = new Company();
+
+
+        Company.companyName = "Orange School";
+        //c1.companyName = "Orange School"; // error 
+
+        Console.WriteLine(Company.companyName);
+       // Console.WriteLine(c1.companyName); // error 
+
+    }
+}
+
+public class Company
+{
+    public static string companyName { get; set; } = "Orange";
+    public string employeeName { get; set; }
+}
