551e1fa9c80e49777594a2f31af85b0001c4deda
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:16:30 +0300

    C# OOP MathUtilities Class

---
+++ BACK_END/C#/C#_OOP_MathUtilities_Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MathUtilities.findMax();
+        MathUtilities.calSqrRoot();
+        MathUtilities.claSqr();
+    
+    
+    }
+}
+
+public static class MathUtilities
+{
+    public static void  claSqr() { }
+    public static void calSqrRoot() { }
+    public static void findMax() { }
+
+    
+}
