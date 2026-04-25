9d15152ad1111a41e2c94f8de120c5af6798d144
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:02:57 +0300

    C# OOP Animal Class

---
+++ BACK_END/C#/C#_OOP_Animal_Class/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { }
+}
+public class Animal
+{
+    public virtual void makeSound() { }
+}
+
+public class Dog :Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat :Animal
+{
+    public override void makeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
