60c07fb2651a35bf3d1e55f425748f6302b769e4
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:04:11 +0300

    C# OOP Animal Shape Person Classes

---
+++ BACK_END/C#/C#_OOP_Animal_Shape_Person_Classes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { }
+}
+/* Task 1 */
+public class Father
+{
+    public void AddMethod (int n1 , int n2)
+    {
+        Console.WriteLine(n1+ n2);
+    }
+    public void AddMethod(int n1, int n2, int n3)
+    {
+        Console.WriteLine(n1 + n2 + n3);
+    }
+
+}
+
+
+/* Task 2 */
+public class Animal
+{
+    public virtual void speak()
+    {
+        Console.WriteLine("Speak From Animal");
+    }
+}
+
+public class Cat :Animal 
+{
+    public override  void speak()
+    {
+
+        Console.WriteLine("Speak From Cat");
+    
+    }
+}
+
+
+/* Task 3 */
+public class Person
+{
+    private string name;
+
+    public string getSetName
+    {
+        get { return name; }
+        set { name = value; }
+    }
+      
+}
+/* Task 5  + 6  */
+
+public class Shape
+{
+    public void getArea() { }
+}
+
+public class Rectangle :Shape { }
