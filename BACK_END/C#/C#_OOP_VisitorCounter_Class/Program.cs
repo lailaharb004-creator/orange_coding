2916ae4375df6665871ae579b013e2640894564b
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:22:34 +0300

    C# OOP VisitorCounter Class

---
+++ BACK_END/C#/C#_OOP_VisitorCounter_Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(VisitorCounter.counter);
+
+        VisitorCounter vistor_1 = new VisitorCounter();
+        VisitorCounter vistor_2 = new VisitorCounter();
+        VisitorCounter vistor_3 = new VisitorCounter();
+        VisitorCounter vistor_4 = new VisitorCounter();
+
+        Console.WriteLine(VisitorCounter.counter);
+
+
+
+    }
+}
+
+public class VisitorCounter
+{
+    public  static int counter = 0;
+
+    public VisitorCounter()
+    {
+        counter++;
+    }
+}
