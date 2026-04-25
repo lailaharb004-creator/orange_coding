f183546af0b78fadf260bf92dc8cd3fe62e84be2
lailaharb004-creator <lailaharb004@gmail.com>
2026-04-25 22:20:52 +0300

    C# OOP Converter Class

---
+++ BACK_END/C#/C#_OOP_Converter_Class/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    { 
+     Converter conv_1 = new Converter();
+        // conv_1.convertCelToFeh(); // error 
+
+        Converter.convertCelToFeh();
+        Converter.convertkiloToMiles();
+
+
+
+
+
+    }
+}
+
+public class Converter
+{
+    public static void convertCelToFeh() { }
+    public static void convertkiloToMiles() { }
+}
